389929b77a59f0dd531a61e804caa043c00b7f63
DumchevEvgeniy <souland@mail.ru>
2019-06-22 16:23:27 +0300

    added server api project

---
+++ TulaRouteSearcherAPI/TulaRouteSearcherAPI/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TulaRouteSearcherAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class TestController : Controller
+    {
+        public TestController()
+        {
+
+        }
+
+        /// <summary>
+        /// TEST method
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(nameof(GetTestData))]
+        public async Task<IActionResult> GetTestData()
+        {
+            return await Task.FromResult(Ok(new { TestData = "Hello" }));
+        }
+
+
+    }
+}
---
+++ TulaRouteSearcherAPI/TulaRouteSearcherAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Examples;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
+
+namespace TulaRouteSearcherAPI.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddStores(this IServiceCollection services, string connectionString)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "API", Version = "v1" });
+                c.OperationFilter<ExamplesOperationFilter>();
+                c.OperationFilter<DescriptionOperationFilter>();
+                var xmlPath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "TulaRouteSearcherAPI.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+    }
+}
