6bd2346b190544d17e8636ed4efc62342c7cee55
Basalov Yurij <basalov_yurij@mail.ru>
2019-06-23 09:27:07 +0300

    cors 3

--- TulaRouteSearcherAPI/TulaRouteSearcherAPI/Startup.cs
+++ TulaRouteSearcherAPI/TulaRouteSearcherAPI/Startup.cs
@@ -20,6 +20,8 @@ namespace TulaRouteSearcherAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOptions();
+            services.AddCors(o => 
+                o.AddPolicy("AllowAll", b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
             services.AddMvc();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.ConfigureSwagger();
@@ -33,30 +35,11 @@ namespace TulaRouteSearcherAPI
                 app.UseDeveloperExceptionPage();
 
             app.UseDeveloperExceptionPage();
+            app.UseCors();
             app.UseMvc();
-            app.UseMiddleware<CorsMiddleware>();
             app.UseStaticFiles();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1"));
-        }
-    }
-
-    public class CorsMiddleware
-    {
-        private readonly RequestDelegate _next;
-
-        public CorsMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public Task Invoke(HttpContext httpContext)
-        {
-            httpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-            httpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-            httpContext.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name");
-            httpContext.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET,PUT,PATCH,DELETE,OPTIONS");
-            return _next(httpContext);
         }
     }
 }
