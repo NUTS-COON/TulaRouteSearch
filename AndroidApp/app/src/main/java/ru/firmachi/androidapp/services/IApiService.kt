eff0b71f93048d977249d953387168369626b708
NUTS-COON <mburavczov@gmail.com>
2019-06-28 22:46:14 +0300

    correct error handling

--- AndroidApp/app/src/main/java/ru/firmachi/androidapp/services/IApiService.kt
+++ AndroidApp/app/src/main/java/ru/firmachi/androidapp/services/IApiService.kt
@@ -1,8 +1,6 @@
 package ru.firmachi.androidapp.services
 
-import io.reactivex.Observable
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 import ru.firmachi.androidapp.models.*
@@ -10,11 +8,11 @@ import ru.firmachi.androidapp.models.*
 interface IApiService {
 
     @POST("/api/RouteSearcher/GetSuggestions")
-    fun getAddressSuggestions(@Body text: SuggestionsAddressRequestModel): Call<List<SuggestionsAddress>>
+    fun getAddressSuggestions(@Body text: SuggestionsAddressRequestModel): Call<List<SuggestionsAddress>?>
 
     @POST("/api/RouteSearcher/GetRoutes")
-    fun getRoutes(@Body routeRequestModel: RouteRequestModel): Observable<Response<List<RoutesResponseModel>?>>
+    fun getRoutes(@Body routeRequestModel: RouteRequestModel): Call<List<RoutesResponseModel>?>
 
     @POST("/api/RouteSearcher/GetCoordinateByHereLocation")
-    fun getCoordinateByHereLocation(@Body location: GetCoordinateRequestModel): Observable<Response<Location?>>
+    fun getCoordinateByHereLocation(@Body location: GetCoordinateRequestModel): Call<Location?>
 }
--- AndroidApp/app/src/main/java/ru/firmachi/androidapp/viewModels/MapViewModel.kt
+++ AndroidApp/app/src/main/java/ru/firmachi/androidapp/viewModels/MapViewModel.kt
@@ -10,6 +10,8 @@ import ru.firmachi.androidapp.App
 import ru.firmachi.androidapp.models.*
 import ru.firmachi.androidapp.services.IApiService
 import javax.inject.Inject
+import okhttp3.ResponseBody
+import retrofit2.Response
 
 
 class MapViewModel : ViewModel() {
@@ -26,62 +28,108 @@ class MapViewModel : ViewModel() {
 
     fun ready(addressFrom: SuggestionsAddress, addressTo: SuggestionsAddress){
 
+//        val handler = Dispatchers.IO + CoroutineExceptionHandler { _, exception ->
+//            GlobalScope.launch(Dispatchers.Main) {
+//                routeLiveData.value = null
+//            }
+//        }
+//
+//        GlobalScope.launch(handler) {
+//            val fromTask = async {
+//                Thread.sleep(4000)
+//                if(addressFrom.coordinate != null){
+//                    return@async addressFrom.coordinate
+//                }
+//                val res = apiService.getCoordinateByHereLocation(GetCoordinateRequestModel(addressFrom.hereLocationId)).execute()
+//                return@async res.body()
+//            }
+//            val toTask = async {
+//                if(addressTo.coordinate != null){
+//                    return@async addressTo.coordinate
+//                }
+//                val res = apiService.getCoordinateByHereLocation(GetCoordinateRequestModel(addressTo.hereLocationId)).execute()
+//                return@async res.body()
+//            }
+//
+//            val from = fromTask.await()
+//            val to = toTask.await()
+//
+//            if(from != null && to != null){
+//                addressFrom.coordinate = from
+//                addressTo.coordinate = to
+//                val res = apiService.getRoutes(RouteRequestModel(null,
+//                    Address(addressFrom.coordinate!!, addressFrom.hereLocationId),
+//                    Address(addressTo.coordinate!!, addressTo.hereLocationId)))
+//                    .execute()
+//                withContext(Dispatchers.Main){
+//                    routeLiveData.value = res.body()
+//                }
+//            }else{
+//                withContext(Dispatchers.IO){
+//                    routeLiveData.value = null
+//                }
+//            }
+//        }
+
         val coordinateFrom = getLocation(addressFrom)
         val coordinateTo = getLocation(addressTo)
 
-        Observable.zip(coordinateFrom, coordinateTo, BiFunction<Location?, Location?, Pair<SuggestionsAddress?, SuggestionsAddress?>>{t1, t2 ->
-            if(t1 == null || t2 == null) return@BiFunction Pair(null, null)
-            addressFrom.coordinate = t1
-            addressTo.coordinate = t2
-            if(addressFrom.coordinate == null || addressTo.coordinate == null){
-                return@BiFunction Pair(null, null)
-            }else{
-                return@BiFunction Pair(addressFrom, addressTo)
-            }
+        Observable.zip(coordinateFrom, coordinateTo, BiFunction<Response<Location?>, Response<Location?>, Pair<SuggestionsAddress?, SuggestionsAddress?>>{t1, t2 ->
+            val res1 = t1.body()
+            val res2 = t2.body()
+
+            if(res1 == null || res2 == null) return@BiFunction Pair(null, null)
+
+            addressFrom.coordinate = res1
+            addressTo.coordinate = res2
+            return@BiFunction Pair(addressFrom, addressTo)
 
         })
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe { pair ->
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .onErrorReturnItem(Pair(null, null))
+            .subscribe { pair ->
 
-            if(pair.first == null || pair.second == null){
-                routeLiveData.value = null
-            }else{
-                val aFrom = pair.first
-                val aTo = pair.second
-                coordinateLiveData.value = aFrom!!.coordinate
+                if(pair.first == null || pair.second == null){
+                    routeLiveData.value = null
+                }else{
+                    val aFrom = pair.first
+                    val aTo = pair.second
+                    coordinateLiveData.value = aFrom!!.coordinate
 
-                val from = Address(aFrom.coordinate!!, aFrom.hereLocationId)
-                val to = Address(aTo?.coordinate!!, aTo.hereLocationId)
+                    val from = Address(aFrom.coordinate!!, aFrom.hereLocationId)
+                    val to = Address(aTo?.coordinate!!, aTo.hereLocationId)
 
-                apiService.getRoutes(RouteRequestModel(null, from, to))
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        if(it.isSuccessful){
-                            routeLiveData.value = it.body()
-                        }else{
-                            routeLiveData.value = null
-                        }
+                    Observable.fromCallable {
+                        return@fromCallable apiService.getRoutes(RouteRequestModel(null, from, to)).execute()
                     }
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe {
+                            if(it.isSuccessful){
+                                routeLiveData.value = it.body()
+                            }else{
+                                routeLiveData.value = null
+                            }
+                        }
+                }
             }
-        }
     }
 
 
-    private fun getLocation(address: SuggestionsAddress): Observable<Location?>{
+    private fun getLocation(address: SuggestionsAddress): Observable<Response<Location?>>{
         if(address.coordinate != null){
-            return Observable.fromCallable {
-                return@fromCallable address.coordinate
-            }
+            return Observable.just(Response.success(address.coordinate))
         }else{
-            return apiService
+            val request = apiService
                 .getCoordinateByHereLocation(GetCoordinateRequestModel(address.hereLocationId))
+
+            return Observable.fromCallable {
+                return@fromCallable request.execute()
+            }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .flatMap {
-                    return@flatMap Observable.fromCallable { return@fromCallable it.body() }
-                }
+                .onErrorReturnItem(Response.error<Location?>(400, ResponseBody.create(null, "")))
         }
     }
 
